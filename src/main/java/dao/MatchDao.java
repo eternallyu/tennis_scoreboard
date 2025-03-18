72879205f8cbad1b6c45e64f5250b99295efb455
eternallyu <zenq7q@gmail.com>
2025-03-18 16:26:00 +0400

    fix

--- src/main/java/dao/MatchDao.java
+++ src/main/java/dao/MatchDao.java
@@ -20,10 +20,8 @@ import static service.FinishedMatchService.PAGE_SIZE;
 public class MatchDao implements Dao<Long, Match> {
 
     public static final MatchDao MATCH_DAO = new MatchDao();
-    public static final String FROM_MATCH_HQL = "from Match";
-    public static final String FROM_MATCH_PAGINATION_HQL = "from Match " +
-                                                           "where player1.name ilike :filterByPlayerName " +
-                                                           "or player2.name ilike :filterByPlayerName";
+    public static final String FROM_MATCH = "from Match";
+    public static final String FROM_MATCH_BY_NAME_HQL = "from Match where player1.name ilike :filterByPlayerName or player2.name ilike :filterByPlayerName";
 
     @Override
     public void save(Match entity) {
@@ -44,7 +42,7 @@ public class MatchDao implements Dao<Long, Match> {
     @Override
     public List<Match> findAll() {
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            return session.createQuery("from Match", Match.class).list();
+            return session.createQuery(FROM_MATCH, Match.class).list();
         } catch (HibernateException exception) {
             throw new DatabaseException("Database error.");
         }
@@ -63,7 +61,7 @@ public class MatchDao implements Dao<Long, Match> {
     public List<Match> findAllPaginated(int offset, int maxResults) {
         try (Session session = HibernateUtil.getSessionFactory().getCurrentSession()) {
             session.beginTransaction();
-            List<Match> finishedMatches = session.createQuery(FROM_MATCH_HQL, Match.class).setFirstResult(offset).setMaxResults(maxResults).list();
+            List<Match> finishedMatches = session.createQuery(FROM_MATCH, Match.class).setFirstResult(offset).setMaxResults(maxResults).list();
             session.getTransaction().commit();
             return finishedMatches;
         } catch (HibernateException exception) {
@@ -74,7 +72,7 @@ public class MatchDao implements Dao<Long, Match> {
     public List<Match> findFilteredByPlayerNamePaginated(int offset, int maxResults, String filterByPlayerName) {
         try (Session session = HibernateUtil.getSessionFactory().getCurrentSession()) {
             session.beginTransaction();
-            List<Match> filteredFinishedMatches = session.createQuery(FROM_MATCH_PAGINATION_HQL, Match.class)
+            List<Match> filteredFinishedMatches = session.createQuery(FROM_MATCH_BY_NAME_HQL, Match.class)
                     .setParameter("filterByPlayerName", "%" + filterByPlayerName + "%")
                     .setFirstResult(offset)
                     .setMaxResults(maxResults)
